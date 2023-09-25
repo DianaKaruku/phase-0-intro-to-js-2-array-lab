4a3c0d2cc6b3db1587c7aefab759e14587f35361
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-09-26 00:52:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+function appendCat(){
+    const newCatsArray=cats.concat("Broom");
+    return newCatsArray;
+}
+function prependCat(){
+    const newCatsArray=["Arnold", ...cats];
+    return newCatsArray;
+}
+function removeLastCat(){
+    const newCatsArray=cats.slice(0,-1);
+    return newCatsArray;
+}
+function removeFirstCat(){
+    const newCatsArray=cats.slice(1);
+    return newCatsArray;
+}
